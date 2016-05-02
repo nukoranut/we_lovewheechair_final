14bb24647a9b89cf4a87cdf87968898556759de2
oranut <oranut_nuk@hotmail.com>
2016-05-02 11:37:45 +0700

    ลงมาคเกอร์ แผนที่แล้ว

--- app/src/main/java/appewtc/masterung/welovewheelchair/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/welovewheelchair/MapsActivity.java
@@ -1,18 +1,33 @@
 package appewtc.masterung.welovewheelchair;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static  double centerLatAdoule = 14.850893;
+    private static  double centerLngAdoule = 101.217796;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +37,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    } // main หลัก
+    //Create Inner class
+    public class ConnectedJSON extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/nuk/php_get_shop.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("2May", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                String[] ShopNameStrings = new String[jsonArray.length()];
+                String[] AddressStrings = new String[jsonArray.length()];
+                String[] PhoneStrings = new String[jsonArray.length()];
+                String[] IconStrings = new String[jsonArray.length()];
+                String[] LatStrings = new String[jsonArray.length()];
+                String[] LngStrings = new String[jsonArray.length()];
+                String[] CategoryStrings = new String[jsonArray.length()];
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    ShopNameStrings[i] = jsonObject.getString("ShopName");
+                    AddressStrings[i] = jsonObject.getString("Address");
+                    PhoneStrings[i] = jsonObject.getString("Phone");
+                    IconStrings[i] = jsonObject.getString("Icon");
+                    LatStrings[i] = jsonObject.getString("Lat");
+                    LngStrings[i] = jsonObject.getString("Lng");
+                    CategoryStrings[i] = jsonObject.getString("Category");
+
+                    // cretea all maker
+                    double lat = Double.parseDouble(LatStrings[i]);
+                    double lag = Double.parseDouble(LngStrings[i]);
+                    LatLng latLng = new LatLng(lat,lag);
+                    mMap.addMarker(new MarkerOptions()
+                            .position(latLng));
+
+
+
+                }   // for
+
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }   // onPost
+
+    }   // ConnectedJSON Class
+
+    public void clickListShop (View view){
+        startActivity(new Intent(MapsActivity.this,ChooseSection.class));
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+        //set up Thai
+        LatLng latLng = new LatLng(centerLatAdoule,centerLngAdoule);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,6));
+
+        ConnectedJSON connectedJSON = new ConnectedJSON();
+        connectedJSON.execute();
+    } // onMap
+
+
+} // คลาสซ้อนคลาส
+
--- app/src/main/java/appewtc/masterung/welovewheelchair/ShopWheelChairActivity.java
+++ app/src/main/java/appewtc/masterung/welovewheelchair/ShopWheelChairActivity.java
@@ -22,75 +22,11 @@ public class ShopWheelChairActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_wheel_chair);
 
-        ConnectedJSON connectedJSON = new ConnectedJSON();
-        connectedJSON.execute();
 
     }   // Main Method
 
     //Create Inner Class
-    public class ConnectedJSON extends AsyncTask<Void, Void, String> {
 
-        @Override
-        protected String doInBackground(Void... voids) {
-
-            try {
-
-                OkHttpClient okHttpClient = new OkHttpClient();
-                Request.Builder builder = new Request.Builder();
-                Request request = builder.url("http://swiftcodingthai.com/nuk/php_get_shop.php").build();
-                Response response = okHttpClient.newCall(request).execute();
-                return response.body().string();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-
-        }   // doInBack
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            Log.d("29April", "JSON ==> " + s);
-
-            try {
-
-                JSONArray jsonArray = new JSONArray(s);
-
-                String[] ShopNameStrings = new String[jsonArray.length()];
-                String[] AddressStrings = new String[jsonArray.length()];
-                String[] PhoneStrings = new String[jsonArray.length()];
-                String[] IconStrings = new String[jsonArray.length()];
-                String[] LatStrings = new String[jsonArray.length()];
-                String[] LngStrings = new String[jsonArray.length()];
-                String[] CategoryStrings = new String[jsonArray.length()];
-
-                for (int i = 0; i < jsonArray.length(); i++) {
-
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    ShopNameStrings[i] = jsonObject.getString("ShopName");
-                    AddressStrings[i] = jsonObject.getString("Address");
-                    PhoneStrings[i] = jsonObject.getString("Phone");
-                    IconStrings[i] = jsonObject.getString("Icon");
-                    LatStrings[i] = jsonObject.getString("Lat");
-                    LngStrings[i] = jsonObject.getString("Lng");
-                    CategoryStrings[i] = jsonObject.getString("Category");
-
-                }   // for
-
-                ListView listView = (ListView) findViewById(R.id.listView);
-                ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(ShopWheelChairActivity.this,
-                        android.R.layout.simple_list_item_1, ShopNameStrings);
-                listView.setAdapter(stringArrayAdapter);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }   // onPost
-
-    }   // ConnectedJSON Class
 
     public void clickBackShopWheelChair(View view) {
         finish();
